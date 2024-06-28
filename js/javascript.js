00a19f2f9f45e647e945037de4fcfe19f9589489
lucadisanto7 <lucadisanto47@gmail.com>
2024-06-28 15:24:40 +0200

    [UPD] Declared array and email variable

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+// DICHIARO L'ARRAY
+const mail_list =
+    ["luttrellajeya@pinopatent.com", 
+    "daimosforyou@suksesboss.com", 
+    "nastynabiiva@chahcyrans.com", 
+    "cpgm4x4@fbclone.com", 
+    "artemslobozhaninov@aulaoberta.es"]
+    
+// DICHIARO LA VARIABILE  
+let email = prompt("inserisci la tua email")                    
